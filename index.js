33a06cc5d88105a0b30106f631dd3c6a88bbd92d
Timothy Nguyen <ngoc.tan@beehexa.com>
2022-05-21 03:34:56 +0700

    fix: deploy error

--- index.js
+++ index.js
@@ -19,33 +19,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+io.sockets.on('connection', (socket) => {
+  console.log('user connected')
+
+  socket.on('on-donate', (data) => {
+    // console.log(data)
+    io.sockets.emit('user-donate', data)
   })
 
-  io.sockets.on('connection', (socket) => {
-    console.log('user connected')
-
-    socket.on('on-donate', (data) => {
-      // console.log(data)
-      io.sockets.emit('user-donate', data)
-    })
-
-    socket.on('on-donate-countdown', (data) => {
-      let counter = data.counter;
-      let WinnerCountdown = setInterval(() => {
-        console.log("chay", counter)
-        io.sockets.emit('counter', counter);
-        counter--
-        if (counter === 0) {
-          io.sockets.emit('user-donate-finish')
-          clearInterval(WinnerCountdown);
-        }
-      }, 1000);
-    })
+  socket.on('on-donate-countdown', (data) => {
+    let counter = data.counter;
+    let WinnerCountdown = setInterval(() => {
+      console.log("chay", counter)
+      io.sockets.emit('counter', counter);
+      counter--
+      if (counter === 0) {
+        io.sockets.emit('user-donate-finish')
+        clearInterval(WinnerCountdown);
+      }
+    }, 1000);
   })
-});
-
+})
 
 
 // app.listen(port, () => {
